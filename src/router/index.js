7de0e3eeac29efd732eb0ca2594e90bce3de2200
张文培 <zhangwenpei@houbi.com>
2018-12-29 15:49:19 +0800

    登录注册

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/home'
+import Auth from '@/views/auth'
+import Login from '@/views/login'
+import Register from '@/views/register'
 Vue.use(Router)
 
 export default new Router({
@@ -14,6 +17,18 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    }, {
+      path: '/auth',
+      name: 'Auth',
+      component: Auth
+    }, {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    }, {
+      path: '/register',
+      name: 'Register',
+      component: Register
     }
   ]
 })
