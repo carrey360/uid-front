d22efeae901b0ed69e05f99fc49458558f169fa0
孙辉 <sunhui@huobi.com>
2018-12-29 11:44:10 +0800

    钱包页面添加

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/home'
+import Wallet from '@/views/wallet'
 Vue.use(Router)
 
 export default new Router({
@@ -14,6 +15,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/wallet',
+      name: 'Wallet',
+      component: Wallet
     }
   ]
 })
